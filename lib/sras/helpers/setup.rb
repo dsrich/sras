7d6d66a7a25d78e1a8c6e5c24e1357c0a902c178
Dave Coyle <hello@coyled.com>
2012-01-13 14:15:52 -0500

    include change that shouldve been included in b6c46ba

--- lib/sras/helpers/setup.rb
+++ lib/sras/helpers/setup.rb
@@ -1,3 +1,3 @@
 RACK_ENV = ENV['RACK_ENV'] || 'production'
 @dbms = ENV['DBMS'] || 'mysql'
-@config = YAML.load_file(::File.dirname(__FILE__) + '/../config/config.yml')
+@config = YAML.load_file("#{ROOT}/config/config.yml")
