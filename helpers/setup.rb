febadc8c4ae57f7d38717e78b0a53f6c03c5c9f5
Dave Coyle <hello@coyled.com>
2012-01-13 01:09:13 -0500

    default to production environment

--- helpers/setup.rb
+++ helpers/setup.rb
@@ -1,3 +1,3 @@
-RACK_ENV = ENV['RACK_ENV'] || 'test'
+RACK_ENV = ENV['RACK_ENV'] || 'production'
 @dbms = ENV['DBMS'] || 'mysql'
 @config = YAML.load_file(::File.dirname(__FILE__) + '/../config/config.yml')
