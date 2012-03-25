52a5417bb645edbdfe01ca7505b31cf926cbeb39
Dave Coyle <hello@coyled.com>
2012-03-25 18:09:15 -0400

    make config file loading a little more sane

--- lib/sras/helpers/config.rb
+++ lib/sras/helpers/config.rb
@@ -1,9 +1,20 @@
-def config_file()
-    if File.exists?("#{ROOT}/config/config.yml")
-        return "#{ROOT}/config/config.yml"
-    elsif File.exists?(File.expand_path('~/.srasrc'))
-        return File.expand_path('~/.srasrc')
-    else
-        return '/etc/sras/sras.conf'
+#
+# load config file
+#
+
+def load_config_file(config_file)
+    unless File.exists?(config_file)
+        if File.exists?("#{ROOT}/config/config.yml")
+            config_file = "#{ROOT}/config/config.yml"
+        elsif File.exists?(File.expand_path('~/.srasrc'))
+            config_file = File.expand_path('~/.srasrc')
+        elsif File.exists('/etc/sras/sras.conf')
+            config_file = '/etc/sras/sras.conf'
+        else
+            warn "Couldn't find SRAS config file.  Exiting."
+            exit 1
+        end
     end
+
+    return YAML.load_file(config_file)
 end
