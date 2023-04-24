812ca9c077ff0a9e22c5100df5d24f5f61469787
Dave <dsrich@hotmail.com>
2023-04-24 12:25:09 -0400

    Create local repo

---
+++ sras_rust/target/debug/deps/sras_rust-ae87fed0a0798a80.d
@@ -0,0 +1,5 @@
+/home/dsrich/rust/sras/sras_rust/target/debug/deps/sras_rust-ae87fed0a0798a80.rmeta: src/main.rs
+
+/home/dsrich/rust/sras/sras_rust/target/debug/deps/sras_rust-ae87fed0a0798a80.d: src/main.rs
+
+src/main.rs:
